f18df2ea0816413c924486de5af25e6ae1e4c42c
alexander gubernikoff <agubernikoff@gmail.com>
2022-01-06 21:13:37 -0500

    added images, startup sequence

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+const shimg1 = document.querySelector(".image1");
+const shgif = document.querySelector(".gif");
+const shimg2 = document.querySelector(".image2");
+const sh1 = document.querySelector(".heezy");
+const spew = document.querySelector(".for-now");
+
+const start = function () {
+  shimg1.classList.add("hidden");
+  shgif.classList.remove("hidden");
+  sh1.innerHTML = "WHEN ANIMATION STOPS CLICK THE IMAGE TO ENTER";
+};
+const start2 = function () {
+  shgif.classList.add("fade");
+  //shimg2.classList.remove("hidden");
+  sh1.classList.add("tran");
+  sh1.innerHTML = "WELCOME";
+  spew.classList.remove("unavailable");
+};
+/*const start3 = function () {
+  shgif.classList.add("fade");
+  sh1.classList.add("tran");
+  sh1.innerHTML = "WELCOME";
+};*/
+shimg1.addEventListener("click", function () {
+  start();
+});
+shgif.addEventListener("click", function () {
+  start2();
+});
+//document.addEventListener("click", function () {
+//  start3();
+//});
