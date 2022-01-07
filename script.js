c4bdb14b888c850c3fa0d6735afc2bcde641f9ab
alexander gubernikoff <agubernikoff@gmail.com>
2022-01-07 12:28:38 -0500

    added greeting array for h1 innerHTML

--- script.js
+++ script.js
@@ -1,32 +1,29 @@
 const shimg1 = document.querySelector(".image1");
 const shgif = document.querySelector(".gif");
-const shimg2 = document.querySelector(".image2");
 const sh1 = document.querySelector(".heezy");
 const spew = document.querySelector(".for-now");
+const greeting = [
+  "CLICK THE IMAGE TO BEGIN",
+  "WHEN THE ANIMATION STOPS CLICK THE IMAGE TO ENTER",
+  "WELCOME",
+];
+
+sh1.innerHTML = greeting[0];
 
 const start = function () {
   shimg1.classList.add("hidden");
   shgif.classList.remove("hidden");
-  sh1.innerHTML = "WHEN ANIMATION STOPS CLICK THE IMAGE TO ENTER";
+  sh1.innerHTML = greeting[1];
 };
 const start2 = function () {
   shgif.classList.add("fade");
-  //shimg2.classList.remove("hidden");
   sh1.classList.add("tran");
-  sh1.innerHTML = "WELCOME";
+  sh1.innerHTML = greeting[2];
   spew.classList.remove("unavailable");
 };
-/*const start3 = function () {
-  shgif.classList.add("fade");
-  sh1.classList.add("tran");
-  sh1.innerHTML = "WELCOME";
-};*/
 shimg1.addEventListener("click", function () {
   start();
 });
 shgif.addEventListener("click", function () {
   start2();
 });
-//document.addEventListener("click", function () {
-//  start3();
-//});
